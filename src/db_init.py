13d663eb030fa82cf670b2919cb0c3f0a20b9782
PapaJool <shakirowkamil2001@gmail.com>
2024-04-23 18:46:30 +0300

    fix

--- src/db_init.py
+++ src/db_init.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3 -u
-
 import os
-import datetime  # Add datetime module
-
 import clickhouse_connect
 import pandas as pd
 from typing import Dict
@@ -12,8 +8,8 @@ class Database():
     def __init__(self):
         host = os.getenv('CLICKHOUSE_HOST', 'clickhouse')
         port = int(os.getenv('CLICKHOUSE_PORT', '8123'))
-        username = os.getenv('CLICKHOUSE_LOGIN', 'default')
-        password = os.getenv('CLICKHOUSE_PWD', '0000')
+        username = os.getenv('CLICKHOUSE_LOGIN')
+        password = os.getenv('CLICKHOUSE_PWD')
         self.client = clickhouse_connect.get_client(host=host, username=username, port=port, password=password)
 
     def create_database(self, database_name="lab2_bd"):
@@ -23,14 +19,13 @@ class Database():
         cols = ""
         for k, v in columns.items():
             cols += f"`{k}` {v}, "
-        id_column = list(filter(lambda i: 'Id' in i[0], columns.items()))[0][0]
         self.client.command(f"""
             CREATE TABLE IF NOT EXISTS {table_name} 
             (
                 {cols}
                 `timestamp` DateTime('UTC') DEFAULT now()
             ) ENGINE = MergeTree
-            ORDER BY {id_column};
+            ORDER BY tuple();  -- No specific order needed
         """)
 
     def insert_data(self, tablename: str, X, y, predictions):
@@ -57,13 +52,4 @@ class Database():
 if __name__ == '__main__':
     db = Database()
     db.create_database("lab2_bd")
-    db.create_table("predictions", {'X': 'String', 'y': 'String', 'predictions': 'String'})  # Assuming X and y are strings
-    # db.create_table("test2", {'nameId': 'UInt32', 'name3': 'UInt32'})
-    # db.insert_df("test1", pd.DataFrame({"nameId": [1]}))
-    # db.insert_df("test2", pd.DataFrame({"nameId": [1], "name3": [1]}))
-    # print(db.read_table("test1"))
-    # print(db.read_table("test2"))
-
-    # db.drop_table("test1")
-    # db.drop_table("test2")
-    # db.drop_database("lab2_bd")
+    db.create_table("predictions", {'X': 'String', 'y': 'String', 'predictions': 'String', 'timestamp': 'DateTime'})
